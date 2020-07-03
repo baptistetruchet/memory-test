768f524eaf35524f17586d3f90d3ddf1f042db10
baptistetruchet <baptiste@lecloset.fr>
2020-07-03 15:09:46 +0200

    init application

---
+++ app/javascript/packs/dashboard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Dashboard = () => {
+  return null;
+};
+
+document.addEventListener("turbolinks:load", () => {
+  ReactDOM.render(<Dashboard />, document.getElementById("dashboard"));
+});
