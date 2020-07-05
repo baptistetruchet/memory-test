af81ffa5d6bb30e448b20a4b63a0ea555bb9fd2b
baptistetruchet <baptiste.truchet@gmail.com>
2020-07-06 00:34:27 +0200

    add vertical margin on card for responsive

--- app/javascript/components/summary-card.jsx
+++ app/javascript/components/summary-card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const SummaryCard = ({ title, content }) => {
   return (
-    <div className="card">
+    <div className="card my-2">
       <div className="card-body text-center">
         <h5>{title}</h5>
         <p className="h2">{content}</p>
